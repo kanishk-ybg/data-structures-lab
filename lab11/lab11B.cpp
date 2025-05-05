56304c99c9eac5260cc922b979cd4b4df53a240a
kanishk-ybg <kanishk24110118@snuchennai.edu.in>
2025-05-05 08:37:55 +0530

    Add files via upload

---
+++ lab11/lab11B.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+#define SIZE 10
+using namespace std;
+
+class Hashtable{
+    private:
+        int Table[SIZE];
+        bool Occupied[SIZE];
+        int HashFunction(int key){
+            return key % SIZE;
+        }
+    public:
+        Hashtable(){
+            for(int i=0;i<SIZE;i++){
+                Table[i]=-1;
+                Occupied[i]=false;
+            }
+        }//Constructor.
+        void Insert(int key);
+        void Delete(int key);
+        void Search(int key);
+        void Display();
+};
+
+int main(){
+    Hashtable HT;
+    while(true){
+    int choice;
+    cout << endl;
+    cout << "The menu is as follows:- " << endl;
+    cout << "1.Insert " << endl;
+    cout << "2.Delete " << endl;
+    cout << "3.Search " << endl;
+    cout << "4.Display " << endl;
+    cout << "5.Exit " << endl;
+    cout << "Enter your choice:- ";
+    cin >> choice;
+    cout << endl;
+    if(choice==1){
+        int key;
+        cout << "Enter the key:- ";
+        cin >> key;
+        cout << endl;
+        HT.Insert(key);//Calling insert function with key as a parameter.
+    }
+    else if(choice==2){
+        int key;
+        cout << "Enter the key:- ";
+        cin >> key;
+        cout << endl;
+        HT.Delete(key);//Calling delete function with key as a parameter.
+    }
+    else if(choice==3){
+        int key;
+        cout << "Enter the key:- ";
+        cin >> key;
+        cout << endl;
+        HT.Search(key);//Calling search function with key as a parameter.
+    }
+    else if(choice==4){
+        HT.Display();//Calling display function.
+    }
+    else if(choice==5){
+        cout << "Exiting...." << endl;//Exiting the menu programme.
+        break;
+    }
+    }
+}
+//Function definition for insertion.
+void Hashtable::Insert(int key){
+    int Index=HashFunction(key);
+    int i=0;
+    while(Occupied[Index]){
+        Index = ((Index+1)*i) % SIZE;
+        i++;
+        if(Index==SIZE){
+            cout << "Hash Table is full!!" << endl;
+            return;
+        }
+    }
+    Table[Index]=key;
+    Occupied[Index]=true;
+    cout << key << " inserted at index " << Index << endl;
+}
+//Function definition for Deleting.
+void Hashtable::Delete(int key){
+    int Index=HashFunction(key);
+    int i=0;
+    while(Occupied[Index]){
+        if (Table[Index]==key){
+            cout << Table[Index] << " deleted from index " << Index << endl;
+            Table[Index]=-1;
+            Occupied[Index]=false;
+            return;
+        }
+        Index = ((Index+1)*i) % SIZE;
+        i++;
+        if(Index==SIZE){
+            break;
+        }
+    }
+    cout << "Key not found!!" << endl;
+}
+//Function definition for Search.
+void Hashtable::Search(int key){
+    int Index=HashFunction(key);
+    int i=0;
+    while(Occupied[Index]){
+        if (Table[Index]==key){
+            cout << Table[Index] << " found at index " << Index << endl;
+            return;
+        }
+        Index = ((Index+1)*i) % SIZE;
+        i++;
+        if(Index==SIZE){
+            break;
+        }
+    }
+    cout << "Key not found!!" << endl;
+}
+//Function definition for Display.
+void Hashtable::Display(){
+    for (int i=0;i<SIZE;i++){
+        if(Occupied[i]==true){
+            cout << Table[i] << " is the element at index " << i << endl;
+        }
+        else{
+            cout << "Empty at index " << i << endl;
+        }
+    }
+}
